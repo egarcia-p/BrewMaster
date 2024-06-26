476c6d5e9a2a114114e4cb43f945a8afffe7766b
Eugenio Garcia <eugenio5039@gmail.com>
2024-06-26 17:01:42 -0600

    Add - schema and actions for recipe and users

---
+++ src/lib/actions.js
@@ -0,0 +1,119 @@
+"use server";
+
+import { Amplify } from "aws-amplify";
+import { generateClient } from "aws-amplify/api";
+import {
+  createRecipe,
+  createUser,
+  deleteRecipe,
+  deleteUser,
+  updateRecipe,
+  updateUser,
+} from "@/graphql/mutations";
+import config from "../amplifyconfiguration.json";
+import { Difficulty } from "@/models";
+
+Amplify.configure(config);
+
+const client = generateClient();
+
+export async function actionCreateUser(formData) {
+  client.graphql;
+  const user = await client.graphql({
+    query: createUser,
+    variables: {
+      input: {
+        userName: formData.get("userName")?.toString() ?? "",
+        password: formData.get("password")?.toString() ?? "",
+        email: formData.get("email")?.toString() ?? "",
+        regDate: formData.get("regDate")?.toString() ?? "",
+        // Recipes: [],
+      },
+    },
+  });
+  revalidatePath("/");
+  //redirect("/");
+}
+
+export async function actionUpdateUser(formData) {
+  client.graphql;
+  const user = await client.graphql({
+    query: updateUser,
+    variables: {
+      input: {
+        id: formData.get("id")?.toString() ?? "",
+        userName: formData.get("userName")?.toString() ?? "",
+        password: formData.get("password")?.toString() ?? "",
+        email: formData.get("email")?.toString() ?? "",
+        regDate: formData.get("regDate")?.toString() ?? "",
+      },
+    },
+  });
+  revalidatePath("/");
+}
+
+export async function actionDeleteUser(id) {
+  client.graphql;
+  const user = await client.graphql({
+    query: deleteUser,
+    variables: {
+      input: {
+        id: id ?? "",
+      },
+    },
+  });
+  revalidatePath("/");
+}
+
+export async function actionCreateRecipe(formData) {
+  client.graphql;
+  const newRecipe = await client.graphql({
+    query: createRecipe,
+    variables: {
+      input: {
+        name: formData.get("name")?.toString() ?? "",
+        slugName: formData.get("slugName")?.toString() ?? "",
+        description: formData.get("description")?.toString() ?? "",
+        elaborationTime: formData.get("elaborationTime")?.toString() ?? "",
+        brewTime: Number(formData.get("brewTime")?.toString()) ?? 0,
+        difficulty: formData.get("difficulty")?.toString() ?? "", //Difficulty.EASY,
+        userID: formData.get("userName")?.toString() ?? "",
+      },
+    },
+  });
+  revalidatePath("/");
+  //redirect("/");
+}
+
+export async function actionUpdateRecipe(formData) {
+  client.graphql;
+  const newRecipe = await client.graphql({
+    query: updateRecipe,
+    variables: {
+      input: {
+        id: formData.get("id")?.toString() ?? "",
+        name: formData.get("name")?.toString() ?? "",
+        slugName: formData.get("slugName")?.toString() ?? "",
+        description: formData.get("description")?.toString() ?? "",
+        elaborationTime: formData.get("elaborationTime")?.toString() ?? "",
+        brewTime: Number(formData.get("brewTime")?.toString()) ?? 0,
+        difficulty: formData.get("difficulty")?.toString() ?? "", //Difficulty.EASY,
+        userID: formData.get("userName")?.toString() ?? "",
+      },
+    },
+  });
+  revalidatePath("/");
+}
+
+export async function actionDeleteRecipe(id) {
+  client.graphql;
+  const user = await client.graphql({
+    query: deleteRecipe,
+    variables: {
+      input: {
+        id: id ?? "",
+      },
+    },
+  });
+  revalidatePath("/");
+}
---
+++ src/models/schema.js
@@ -0,0 +1,225 @@
+export const schema = {
+    "models": {
+        "Recipe": {
+            "name": "Recipe",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "name": {
+                    "name": "name",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "slugName": {
+                    "name": "slugName",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "description": {
+                    "name": "description",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "elaborationTime": {
+                    "name": "elaborationTime",
+                    "isArray": false,
+                    "type": "AWSTime",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "brewTime": {
+                    "name": "brewTime",
+                    "isArray": false,
+                    "type": "Int",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "difficulty": {
+                    "name": "difficulty",
+                    "isArray": false,
+                    "type": {
+                        "enum": "Difficulty"
+                    },
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "userID": {
+                    "name": "userID",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "createdAt": {
+                    "name": "createdAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                },
+                "updatedAt": {
+                    "name": "updatedAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                }
+            },
+            "syncable": true,
+            "pluralName": "Recipes",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                },
+                {
+                    "type": "key",
+                    "properties": {
+                        "name": "byUser",
+                        "fields": [
+                            "userID"
+                        ]
+                    }
+                },
+                {
+                    "type": "auth",
+                    "properties": {
+                        "rules": [
+                            {
+                                "allow": "public",
+                                "operations": [
+                                    "create",
+                                    "update",
+                                    "delete",
+                                    "read"
+                                ]
+                            }
+                        ]
+                    }
+                }
+            ]
+        },
+        "User": {
+            "name": "User",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "userName": {
+                    "name": "userName",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "password": {
+                    "name": "password",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "email": {
+                    "name": "email",
+                    "isArray": false,
+                    "type": "AWSEmail",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "regDate": {
+                    "name": "regDate",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "Recipes": {
+                    "name": "Recipes",
+                    "isArray": true,
+                    "type": {
+                        "model": "Recipe"
+                    },
+                    "isRequired": false,
+                    "attributes": [],
+                    "isArrayNullable": true,
+                    "association": {
+                        "connectionType": "HAS_MANY",
+                        "associatedWith": [
+                            "userID"
+                        ]
+                    }
+                },
+                "createdAt": {
+                    "name": "createdAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                },
+                "updatedAt": {
+                    "name": "updatedAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                }
+            },
+            "syncable": true,
+            "pluralName": "Users",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                },
+                {
+                    "type": "auth",
+                    "properties": {
+                        "rules": [
+                            {
+                                "allow": "public",
+                                "operations": [
+                                    "create",
+                                    "update",
+                                    "delete",
+                                    "read"
+                                ]
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    },
+    "enums": {
+        "Difficulty": {
+            "name": "Difficulty",
+            "values": [
+                "EASY",
+                "MEDIUM",
+                "HARD"
+            ]
+        }
+    },
+    "nonModels": {},
+    "codegenVersion": "3.4.4",
+    "version": "e4532b055edab1a86d58644c5bede3e1"
+};
